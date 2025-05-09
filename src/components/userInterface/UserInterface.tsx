61d0c0adc8831773f8f6bf092775683427a9f574
Bogdan Leonte <leonte.bogdan.sebastian@gmail.com>
2025-05-10 00:05:49 +0300

    feat(UI): Create user interface box

---
+++ src/components/userInterface/UserInterface.tsx
@@ -0,0 +1,11 @@
+import "./UserInterface.scss";
+
+const UserInterface = () => {
+  return (
+    <div className="user-interface">
+      <span className="user-interface__text">UserInterface</span>
+    </div>
+  );
+};
+
+export default UserInterface;
