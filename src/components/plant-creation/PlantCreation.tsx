b096d1e57d463ec7da0bc5751d515374bec23bb8
Bogdan Leonte <leonte.bogdan.sebastian@gmail.com>
2025-05-15 22:41:39 +0300

    WIP

--- src/components/plant-creation/PlantCreation.tsx
+++ src/components/plant-creation/PlantCreation.tsx
@@ -1,7 +1,18 @@
 import "./PlantCreation.scss";
 
 const PlantComponent = () => {
-  return <div className="user-interface"></div>;
+  return (
+    <div className="user-interface">
+      <div className="window">
+        <div className="window-sill"></div>
+      </div>
+      <div className="bench">
+        <div className="pot-container">
+          <div className="flower-pot" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlantComponent;
